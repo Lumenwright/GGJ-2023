4ff8a674cf5a062b15abf07192d275f0199bbf8b
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2023-02-04 16:19:34 -0800

    Feature: Sun rotation and a central game parameter asset

---
+++ Assets/GameParameters.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameParameters : ScriptableObject
+{
+    public float EarthRadius;
+    // EarthRadius + SunRadiusOffset = SunRadius 
+    // to keep Sun above Earth
+    public float SunRadiusOffset;
+    public float SunSpeed;
+
+}
---
+++ Assets/SunRotation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Rotates this GameObject at a set speed
+public class SunRotation : MonoBehaviour
+{
+    [SerializeField]
+    GameParameters _gameParams;
+
+    [SerializeField]
+    Transform _sunSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float radius = _gameParams.SunRadiusOffset + _gameParams.EarthRadius;
+        _sunSprite.position = Vector2.right * radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = _gameParams.SunSpeed;
+        transform.Rotate(0f, 0f, speed);
+    }
+}
